d08c0a45dd906e608ac89c90f7802518468ce482
andresali1 <andresali1@hotmail.es>
2023-06-26 16:21:28 -0500

    Ajustes poco importantes

--- SQLServerScripts/DDL.sql
+++ SQLServerScripts/DDL.sql
@@ -3,17 +3,3 @@ GO
 
 USE RunningApp
 GO
-
-/*
-Migration Commands
-
-(Package Manager Console)
-Add-Migration InitialCreate
-Update-Database
-Add-Migration Identity
-Add-Migration NullAddressFix
-Add-Migration AddCityStateProfileImage
-
-(PowerShell -> cd Project Root RunningAppNetCore)
-dotnet run seeddata
-*/
